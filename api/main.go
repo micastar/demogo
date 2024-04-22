1898832d3ea73ebf41c5a6a5b8cc54912bd27d6d
micastar <51841519+micastar@users.noreply.github.com>
2024-04-22 03:14:29 +0000

    add funcs setupRoutes and main

--- api/main.go
+++ api/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/micastar/shorten-url/routes"
+)
+
+func setuprRoutes(r *gin.Engine) {
+	r.GET("/:url", routes.ResolveURL)
+	r.POST("/api/v1", routes.ShortenURL)
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	r := gin.New()
+
+	r.Use(gin.Logger())
+
+	setuprRoutes(r)
+
+	log.Fatal(r.Run(os.Getenv("APP_ADDR")))
+}
