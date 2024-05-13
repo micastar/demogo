b67e86222296f4abefc3afc0d4b4fc5354350a52
micastar <51841519+micastar@users.noreply.github.com>
2024-05-13 14:45:52 +0000

    short-url || add tmpl func

--- api/main.go
+++ api/main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"html/template"
+	"io"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/micastar/shorten-url/helpers"
 	"github.com/micastar/shorten-url/routes"
 )
 
 func setupRoutes(r *gin.Engine) {
+	r.GET("/", routes.Homepage)
 	r.GET("/:url", routes.ResolveURL)
 	r.POST("/api", routes.ShortenURL)
 }
@@ -24,7 +29,31 @@ func init() {
 func main() {
 	r := gin.Default()
 
+	t, err := loadTemplate()
+	if err != nil {
+		panic(err)
+	}
+	r.SetHTMLTemplate(t)
+
 	setupRoutes(r)
 
 	log.Fatal(r.Run(os.Getenv("APP_PORT")))
+}
+
+func loadTemplate() (*template.Template, error) {
+	t := template.New("")
+	for name, file := range helpers.Assets.Files {
+		if file.IsDir() || !strings.HasSuffix(name, ".tmpl") {
+			continue
+		}
+		h, err := io.ReadAll(file)
+		if err != nil {
+			return nil, err
+		}
+		t, err = t.New(name).Parse(string(h))
+		if err != nil {
+			return nil, err
+		}
+	}
+	return t, nil
 }
