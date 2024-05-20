f5e6717eb69b4559e9807265b89c83410c0244f3
micastar <51841519+micastar@users.noreply.github.com>
2024-05-20 01:05:18 +0000

    basic-prometheus-grafana || add registerDevices and createDevice funcs

--- my-app/main.go
+++ my-app/main.go
@@ -59,7 +59,7 @@ func main() {
 	m.info.With(prometheus.Labels{"version": version}).Set(1)
 
 	dMux := http.ServeMux{}
-	dMux.HandleFunc("/devices", getDevice)
+	dMux.HandleFunc("/devices", registerDevices)
 
 	pMux := http.ServeMux{}
 	promHandler := promhttp.HandlerFor(reg, promhttp.HandlerOpts{EnableOpenMetrics: true})
@@ -76,6 +76,18 @@ func main() {
 	select {}
 }
 
+func registerDevices(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		getDevice(w, r)
+	case "POST":
+		createDevice(w, r)
+	default:
+		w.Header().Set("Allow", "GET, POST")
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
 func getDevice(w http.ResponseWriter, r *http.Request) {
 	b, err := json.Marshal(dvs)
 	if err != nil {
@@ -85,4 +97,19 @@ func getDevice(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(b)
+}
+
+func createDevice(w http.ResponseWriter, r *http.Request) {
+	var dv Device
+
+	err := json.NewDecoder(r.Body).Decode(&dv)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	dvs = append(dvs, dv)
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Device Created"))
 }
