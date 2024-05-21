d92901eda40adeacbec93801e68a411c11d44379
micastar <51841519+micastar@users.noreply.github.com>
2024-05-21 02:49:20 +0000

    file-to-storage-and-share || add metric code for download

--- pkg/web/downloadHandler.go
+++ pkg/web/downloadHandler.go
@@ -3,19 +3,33 @@ package web
 import (
 	"context"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/render"
 	"github.com/micastar/file-to-storage-and-share/config"
 	"github.com/micastar/file-to-storage-and-share/pkg/db"
+	"github.com/micastar/file-to-storage-and-share/pkg/metric"
+	"github.com/micastar/file-to-storage-and-share/pkg/utils"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/redis/go-redis/v9"
 )
 
-func downloadFile(w http.ResponseWriter, r *http.Request) {
+type downloadHandler struct {
+	metric *metric.Metrics
+}
+
+func (dh *downloadHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	downloadFile(w, r, dh.metric)
+}
+
+func downloadFile(w http.ResponseWriter, r *http.Request, m *metric.Metrics) {
+
+	now := time.Now().UTC()
+
 	var ctx = context.Background()
 	var rdb = db.RedisClient(config.REDIS_DB_MAIN)
 	defer rdb.Close()
@@ -38,27 +52,28 @@ func downloadFile(w http.ResponseWriter, r *http.Request) {
 
 	// Open the file
 	filePath := fileMetadata["path"]
-	file, err := os.Open(filePath)
-	if err != nil {
-		log.Println("file error: ", err)
-		http.Error(w, "Failed to open file", http.StatusInternalServerError)
-		return
-	}
+	file := openFile(filePath, w)
 	defer file.Close()
 
 	// Set the Content-Disposition header to only download
 	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s", fileMetadata["filename"]))
 
-	// Copy file data to response writer
-	_, err = io.Copy(w, file)
+	utils.Copy2Dst(w, w, file)
 
-	if err != nil {
-		log.Println("copy error: ", err)
-		http.Error(w, "Failed to write file data to response", http.StatusInternalServerError)
-		return
-	}
-
+	m.DownloadRequestCounter.With(prometheus.Labels{"type": "download"}).Inc()
+	m.DownloadRequestDuration.Observe(float64(time.Since(now).Seconds()))
 }
+
+func openFile(filePath string, w http.ResponseWriter) *os.File {
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Println("file error: ", err)
+		http.Error(w, "Failed to open file", http.StatusInternalServerError)
+		return nil
+	}
+	return file
+}
+
 func getFileMetadataFromRedis(fileID string, ctx context.Context, rdb *redis.Client) (map[string]string, error) {
 	// Retrieve file metadata from Redis based on file ID
 	fileMetadata, err := rdb.HGetAll(ctx, fileID).Result()
