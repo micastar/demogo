9fbd1620e4b025a976b119b029c7dde1ee4e2909
micastar <51841519+micastar@users.noreply.github.com>
2024-05-21 02:44:42 +0000

    file-to-storage-and-share || edit config configuration

--- config/config_prod.go
+++ config/config_prod.go
@@ -7,15 +7,17 @@ import (
 )
 
 const (
+	// APP
+	DefaultProg = "fss"
 	// REDIS
-	REDIS_HOST     = "172.17.0.2:6379"
+	REDIS_HOST     = "10.5.0.4:6379"
 	REDIS_PWD      = ""
 	REDIS_DB_MAIN  = 0
 	REDIS_DB_INCR  = 1
 	REDIS_TTL_MAIN = 180 * time.Second // 3 Minute
 	REDIS_TTL_INCR = 300 * time.Second // 5 Minute
 	// CHI
-	CHI_ADDR           = "localhost"
+	CHI_ADDR           = "10.5.0.5"
 	CHI_PORT           = "8338"
 	CHI_DefaultTimeOut = 30 * time.Second // 30 Seconds
 )
